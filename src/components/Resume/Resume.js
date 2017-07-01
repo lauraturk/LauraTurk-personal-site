fd18098697b9acbb715d4ee968b3de40d3b0a883
Laura Turk <laura.lk.turk@gmail.com>
2017-07-01 17:57:29 -0600

    troubleshoot resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,7 +3,7 @@ import ReactPDF from 'react-pdf'
 
 const Resume = () => {
   return (
-      <ReactPDF file='/assets/LauraTurk_FE-DEV_2017.pdf'
+      <ReactPDF file="/assets/LauraTurk_FE-DEV_2017.pdf"
                 loading="Loading, Please Wait!"
                 error="Sorry! An error occured"
       />
