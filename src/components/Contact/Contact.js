0ea9a0d8ca82ed8a04da97b3d702b05bc1eaaf20
Laura Turk <laura.lk.turk@gmail.com>
2017-07-04 13:24:31 -0600

    fix grid structure, add jet fuel project, start project page style

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <form action="https://formspree.io/laura.lk.turk@gmail.com"
-          method="POST">
-      <input type="text" name="name" placeholder="Your Name"/>
-      <input type="email" name="_replyto" placeholder="Your Email"/>
-      <input type="textarea" name="text" placeholder="Your Message"/>
-      <input type="submit" value="Send"/>
-    </form>
+    <section className="u-content">
+      <form action="https://formspree.io/laura.lk.turk@gmail.com"
+      method="POST">
+        <input type="text" name="name" placeholder="Your Name"/>
+        <input type="email" name="_replyto" placeholder="Your Email"/>
+        <input type="textarea" name="text" placeholder="Your Message"/>
+        <input type="submit" value="Send"/>
+      </form>
+    </section>
   )
 }
 
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,10 +3,12 @@ import ReactPDF from 'react-pdf'
 
 const Resume = () => {
   return (
+    <section className="u-content">
       <ReactPDF file="/assets/LauraTurk_FE-DEV_2017.pdf"
-                loading="Loading, Please Wait!"
-                error="Sorry! An error occured"
+      loading="Loading, Please Wait!"
+      error="Sorry! An error occured"
       />
+    </section>
   )
 }
 
