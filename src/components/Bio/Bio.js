432a62e7217906137649513277ec0d4fb2a84d96
Laura Turk <laura.lk.turk@gmail.com>
2017-07-06 21:37:52 -0600

    compress pdf

---
+++ src/components/Bio/Bio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Bio = () => {
+  return (
+    <article className="Bio">
+      {/* <h3>About Me</h3> */}
+      <p>
+        I am a detailed, dedicated, life-long learner passionate about
+        creating social good through my work. I came to programming by way of
+        teaching digital literacy and 21st Century Skills in public libraries,
+        and have honed my ability to lead, collaborate and communicate with
+        people from all walks of life.
+      </p>
+    </article>
+  )
+}
+
+export default Bio
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -4,7 +4,7 @@ import ReactPDF from 'react-pdf'
 const Resume = () => {
   return (
     <section className="u-content">
-      <ReactPDF file="/assets/LauraTurk_FE-DEV_2017.pdf"
+      <ReactPDF file="/assets/LauraTurk-FE_DEV-2017.compressed.pdf"
       loading="Loading, Please Wait!"
       error="Sorry! An error occured"
       />
